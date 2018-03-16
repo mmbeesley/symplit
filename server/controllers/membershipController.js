45a79dad6147dbee22a0188f02c8468907338606
FullFaceRub <danielhbury@gmail.com>
2018-03-16 13:42:03 -0600

    finished basic layout of the backend with controllers, sql files, and endpoints

--- server/controllers/membershipController.js
+++ server/controllers/membershipController.js
@@ -1,23 +1,46 @@
 module.exports = {
 
     getMemberships: (req,res) => {
+        const db = req.app.get('db');
 
+        db.get_allmemberships().then(memberships => {
+            res.status(200).send(memberships);
+        })
     },
 
     getOneMembership: (req,res) => {
+        const db = req.app.get('db');
+        const { membershipId } = req.params;
 
+        db.get_membership([membershipId]).then(membership => {
+            res.status(200).send(membership);
+        })
     },
 
     createMembership: (req,res) => {
+        const db = req.app.get('db');
+        const {membership_title, membership_description, membership_price, membership_recurring } = req.body;
 
+        db.create_membership([membership_title, membership_description, membership_price, membership_recurring]).then(newMembership => {
+            res.status(200).send(newMembership)
+        })
     },
 
     updateMembership: (req,res) => {
+        const db = req.app.get('db');
+        const {membership_title, membership_description, membership_price, membership_recurring} = req.body;
+        const {membershipId} = req.params;
 
+        db.update_membership([membershipId, membership_title, membership_description, membership_price, membership_recurring]).then(updated => {
+            res.status(200).send(updated)
+        })
     },
 
     deleteMembership: (req,res) => {
+        const db = req.app.get('db');
+        const {membershipId} = req.params;
 
+        db.delete_membership([membershipId]).then(deleted => {})
     }
-    
+
 }
--- server/controllers/offerController.js
+++ server/controllers/offerController.js
@@ -1,23 +1,46 @@
 module.exports = {
 
     getOffers: (req,res) => {
+        const db = req.app.get('db');
 
+        db.get_alloffers().then(offers => {
+            res.status(200).send(offers)
+        })
     },
 
     getOneOffer: (req,res) => {
+        const db = req.app.get('db');
+        const {offerId} = req.params;
 
+        db.get_offer([offerId]).then(offer => {
+            res.status(200).send(offer);
+        })
     },
 
     createOffer: (req,res) => {
-
+        const db = req.app.get('db');
+        const {offer_title, offer_code, discount} = req.body;
+        
+        db.create_offer([offer_title, offer_code, discount]).then(newOffer => {
+            res.status(200).send(newOffer);
+        })
     },
 
     updateOffer: (req,res) => {
+        const db = req.app.get('db');
+        const {offer_title, offer_code, discount} = req.body;
+        const {offerId} = req.params;
 
+        db.update_offer([offerId, offer_title, offer_code, discount]).then(udpated => {
+            res.status(200).send(udpated);
+        })
     },
 
     deleteOffer: (req,res) => {
+        const db = req.app.get('db');
+        const {offerId} = req.params;
 
+        db.delete_offer([offerId]).then(deleted => {})
     }
-    
+
 }
