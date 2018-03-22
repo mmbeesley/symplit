7426114c4bcb9a429464cce2a952d7c07c28a48b
FullFaceRub <danielhbury@gmail.com>
2018-03-22 16:40:45 -0700

    home page db build file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,47 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class Home extends Component{
-    render(){
-        return(
-            <div>
+class Home extends Component {
+    render() {
+        return (
+            <div className="homebody">
+                <div className="homesplash">
+                    <div className="hometitle">Struggling in Math?</div>
+                    <div className="homesubtitle">Textbook-Specific Tutorial Videos</div>
+                </div>
+                <div className="testimonialcontainer">
+                    <div className='testimonialtitle'>
+                        Student Testimonials
+                    </div>
+                    <div className="testimonialbody">
+                        <div className='testimonialcarousel'>
 
+                        </div>
+                        <button className="testimonialbutton">GET STARTED</button>
+                    </div>
+                </div>
+                <div className="homemissioncontainer">
+                    <div>WE FULLY BELIEVE THAT</div><div className="homemissionaccent"> EVERY STUDENT CAN SUCCEED</div><div> IN MATH; THEY JUST NEED THE RIGHT TEACHER.</div>
+                </div>
+                <div className="howcontainer">
+                    <div className="howimage"></div>
+                    <div className="howbody">
+                        <h1>How We Help You</h1>
+                        <ul>
+                            <li>Content organized by the sections in your textbook</li>
+                            <li>Numerous example videos for each concept</li>
+                            <li>Downloadable PDFs with unique tips and tricks for tackling the harder concepts</li>
+                        </ul>
+                        <button className="testimonialbutton">GET STARTED</button>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-function mapStateToProps(state){
-    
+function mapStateToProps(state) {
+
 }
 
 export default connect(mapStateToProps)(Home)
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import book from '../images/icon-book.png';
+import problem from '../images/icon-problem.png';
+import about from '../images/icon-symplit.png';
 
 export default class NavBar extends Component {
     render() {
         return (
             <div className="navcontainer">
                 <Link to='/books'> <div className="navlink">
-                    <div className="navlinkicon"></div>
+                    <div className="navlinkicon" ><img src={book} alt="Books" className="nav-icon"/></div>
                     <div className="navlinkdesc">Books</div>
                 </div></Link>
                 <Link to='/problems'> <div className="navlink">
-                    <div className="navlinkicon"></div>
+                    <div className="navlinkicon" ><img src={problem} alt="Problems" className="nav-icon"/></div>
                     <div className="navlinkdesc">Problems</div>
                 </div></Link>
-                <Link to='/'> <div className="navlink">
-                    <div className="navlinkicon"></div>
-                    <div className="navlinkdesc">Search</div>
-                </div></Link>
                 <Link to='/about'> <div className="navlink">
-                    <div className="navlinkicon"></div>
+                    <div className="navlinkicon" ><img src={about} alt="About" className="nav-icon"/></div>
                     <div className="navlinkdesc">About</div>
                 </div></Link>
             </div>
